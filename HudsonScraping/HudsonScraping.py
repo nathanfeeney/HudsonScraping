78aab32c64c38a162e5b10dd9d9069189c9e81d7
Thomas Nathan Feeney <31934348+nathanfeeney@users.noreply.github.com>
2021-04-27 17:54:39 +0100

    Beautiful Soup

--- HudsonScraping/HudsonScraping.py
+++ HudsonScraping/HudsonScraping.py
@@ -1,1 +1,5 @@
-
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("https://dev-test.hudsonstaging.co.uk/")
+bsObj = BeautifulSoup(html.read() 'xml')
+print(bsObj.h1)
