b22f9112c2bbc54841f7e3654bd577a0f87a04ad
Thomas Nathan Feeney <31934348+nathanfeeney@users.noreply.github.com>
2021-04-27 20:10:42 +0100

    printing product names

--- HudsonScraping/HudsonScraping.py
+++ HudsonScraping/HudsonScraping.py
@@ -2,4 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("https://dev-test.hudsonstaging.co.uk/")
 bsObj = BeautifulSoup(html.read(), 'lxml')
-print(bsObj.p)
+nameList = bsObj.findAll("p",{"class": "product-name"})
+for name in nameList:
+        print(name.get_text())
+
