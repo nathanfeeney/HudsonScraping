b8aadcd0884b7ba6a21d8b8186d21b28a3fd2377
Thomas Nathan Feeney <31934348+nathanfeeney@users.noreply.github.com>
2021-05-01 14:56:36 +0100

    first 3 indexes now show

--- HudsonScraping/HudsonScraping.py
+++ HudsonScraping/HudsonScraping.py
@@ -1,8 +1,30 @@
+import json
+import urllib
 from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
-html = urlopen("https://dev-test.hudsonstaging.co.uk/")
-bsObj = BeautifulSoup(html.read(), 'lxml')
-nameList = bsObj.findAll("p",{"class": "product-name"})
-for name in nameList:
-        print(name.get_text())
 
+
+url = 'https://dev-test.hudsonstaging.co.uk/'
+response = requests.get(url, timeout=5)
+content = BeautifulSoup(response.content, "html.parser")
+productNameArr = []
+productInfoArr = []
+for tweet in content.findAll('div', attrs={"class": "product-tile"}):
+    productObject = {
+        "product": tweet.find('p', attrs={"class": "product-name"}).text,
+         "meta data": tweet.findAll('p')[1].text,
+         "": tweet.findAll('p')[2].text
+    }
+    productNameArr.append(productObject)
+#for tweet in content.findAll('div', attrs={"class": "details"}):
+   # infoObject = {   
+       
+        #"metadataTest": tweet.findAll(text='Quantity:', items=2)
+    #}
+    
+   # productInfoArr.append(infoObject)
+with open('productNEW.json', 'w') as outfile:
+    json.dump(productNameArr, outfile)
+  #  json.dump(productInfoArr, outfile)
+    print("file created")
