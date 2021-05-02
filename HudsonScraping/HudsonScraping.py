c86bf19f9826ba8da9a39f8aef0d74d70da4d1a1
Thomas Nathan Feeney <31934348+nathanfeeney@users.noreply.github.com>
2021-05-02 23:21:34 +0100

    Final Code
    I was not able to get the string to int conversion to work for the formatting of Quantity and Price.

--- HudsonScraping/HudsonScraping.py
+++ HudsonScraping/HudsonScraping.py
@@ -8,20 +8,22 @@ from bs4 import BeautifulSoup
 url = 'https://dev-test.hudsonstaging.co.uk/'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
-productNameArr = []
 productInfoArr = []
+contentArr = []
 
 
-for tweet in content.findAll('div', attrs={"class": "product-tile"}):
+for info in content.findAll('div', attrs={"class": "product-tile"}):
     productObject = {
-        "product": tweet.find('p', attrs={"class": "product-name"}).text,
-         "meta data": {
-          "Quantity": tweet.findAll('p')[1].text.strip("Quantity: "),
-          "Price": tweet.findAll('p')[2].text.strip("Price: $"),
-          "img-url": tweet.find("img").get('src')
+        "product": info.find('p', attrs={"class": "product-name"}).text,
+        "meta data": {
+         #this finds all <p> tags at the 2rd index and strips the text
+        "Quantity": info.findAll('p')[1].text.strip("Quantity: "),
+          #this finds all <p> tags at the 3rd index and strips the text and currency
+         "Price": info.findAll('p')[2].text.strip("Price: $"),
+          #this finds all <img> tags at gets the src value
+         "img-url": info.find("img").get('src')
     }}
-    productNameArr.append(productObject)
-
-with open('katrina.json', 'w') as outfile:
-    json.dump(productNameArr, outfile, indent=1)
+    productInfoArr.append(productObject)
+with open('productInfo.json', 'w') as outfile:
+    json.dump(productInfoArr, outfile, indent=1)
     print("file created")
